bdb3b36a266095682929ae42f0536c4ee619f423
Parmida-Hooshang <parmida.h.84@gmail.com>
2025-05-18 18:57:21 +0330

    Updates to the structure

--- asm/GoL.asm
+++ asm/GoL.asm
@@ -1,8 +1,7 @@
 #########################################################
-# Name: Fate											#
-# Functionality: Decides whether the cell is alive or	#
-#		 dead in the next stage			#
-# Results: Number of alive neighbors 		        #
+# Name: Fate						#
+# Functionality: Determines the future of a cell	#
+# Result: The cell's state of life 			#
 # Uses: -					        # 
 #########################################################
 Fate:
@@ -10,7 +9,7 @@ Fate:
 
 #########################################################
 # Name: Tomorrow				 	#
-# Functionality: Evaluation of the next state 	        #
+# Functionality: Evaluates the next state 	        #
 # Results: Next state GSA and the Everlasting flag	#
 # Uses: Fate					        # 
 #########################################################
@@ -18,37 +17,28 @@ Tomorrow:
 
 
 #########################################################
-# Name: Equilibrium				 	#
-# Functionality: Comparing today and tomorrow 		#
-# Results: Equality of two GSAs			        #
-# Uses: -		                         	#
-#########################################################
-Equilibrium:
-
-
-#########################################################
-# Name: Today					        #
-# Functionality: Resetting the current state 	        #
-# Results: Swaps two GSAs			        #
-# Uses: -					        #
-#########################################################
-Today:
-
-
-#########################################################
 # Name: Crystal_ball					#
-# Functionality: Preprocessing 			        #
-# Results: Number of iterations, Positions of walls     #
-# Uses: Tomorrow, Equilibrium                           #
+# Functionality: Unveils the destined conclusion  	#
+# Results: Number of iterations, Position of walls     	#
+# Uses: Tomorrow                           		#
 #########################################################
 Crystal_ball:
 
 
 #########################################################
+# Name: Resurrection					#
+# Functionality: Swaps two GSAs 	        	#
+# Result: Today is yesterday's tomorrow			#
+# Uses: -					        #
+#########################################################
+Resurrection:
+
+
+#########################################################
 # Name: main					        #
-# Functionality: Body of the game		        #
-# Results: Runs the game			        #
-# Uses: Fate, Today, Tomorrow                           #
+# Functionality: Runs the game			        #
+# Result: Flow of life			        	#
+# Uses: Crystal_ball, Tomorrow, Resurrection            #
 #########################################################
 main: 
 	
