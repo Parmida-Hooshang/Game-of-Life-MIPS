4ba40df77b03a15d1cc7f96c4097b0efe0bda072
Parmida-Hooshang <parmida.h.84@gmail.com>
2025-05-21 19:31:18 +0330

    Interpreter: handling labels

--- interpreter/interpreter.js
+++ interpreter/interpreter.js
@@ -7,7 +7,9 @@ class Interpreter {
             t8: 0, t9: 0, k0: 0, k1: 0, gp: 0, sp: 0, fp: 0, ra: 0
         };
         this.labels = {};
+        this.data_labels = {}
         this.memory = new Array(1024).fill(0);
+        this.code = [];
         this.pc = 0;
         this.memory_pointer = 0;
     }
@@ -17,7 +19,7 @@ class Interpreter {
         const name = parts[0].slice(0,-1);
         const type = parts[1];
         const values = parts[2];
-        this.labels[name] = this.memory_pointer;
+        this.data_labels[name] = this.memory_pointer;
         if (type == ".space"){
             this.memory_pointer += parseInt(values);
         }
@@ -31,7 +33,7 @@ class Interpreter {
             }
         }
         else {
-            for (let value of values.split(',')){
+            for (let value of values.split(',')) {
                 this.memory[this.memory_pointer] = parseInt(value);
                 this.memory_pointer += 1;
             }
@@ -39,7 +41,20 @@ class Interpreter {
         
     }
 
+    first_pass() {
+        this.pc = 0;
+        for (let line of this.code) {
+            if (line.includes(':')) {
+                // it's a label
+                this.labels[line.trim().slice(0,-1)] = this.pc + 1;
+            }
+            this.pc += 1;
+        }
+        this.pc = 0;
+    }
+
     execute(instruction) {
+        instruction = instruction.trim();
         // addi
         if (instruction.startsWith('addi')) {
             const parts = instruction.split(/[\s,]+/);
@@ -48,6 +63,15 @@ class Interpreter {
             const imm = parseInt(parts[3]);
             this.registers[rt] = this.registers[rs] + imm;
         }
+
+        // la
+        else if(instruction.startsWith('la')) {
+            const parts = instruction.split(/[\s,]+/);
+            const dest = parts[1].substring(1);
+            const label = parts[2];
+            this.registers[dest] = this.labels[label];
+        }
+
         // Add other instructions...
     }
 }
@@ -61,3 +85,14 @@ console.log(mips.registers.t0);
 mips.declare('origin: 		.word	1,2,3,4,5,6,7,8');
 mips.declare('presence: 		.space	3');
 mips.declare('corpse: 		.word 	-1:2');
+
+let piece_of_code = 
+`Genesis: 
+	la		$a1, presence
+	la		$a0, in_between`;
+
+mips.code = piece_of_code.split('\n');
+mips.first_pass(piece_of_code);
+
+mips.execute('  la  $a1, Genesis');
+console.log(mips.registers.a1);
